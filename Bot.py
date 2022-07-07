cc83314692948e9433ed0941161aea66a8455825
AlkTagar <r0gozinmax@yandex.ru>
2022-07-07 18:50:35 +0300

    just a bot

--- Bot.py
+++ Bot.py
@@ -7,7 +7,11 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(content_types=["text"])
 def get_text_message(message):
-    bot.send_message(message.from_user.id, "ЫЫЫЫ")
+    match message.text:
+        case "/start":
+            bot.send_message(message.from_user.id, "Hello")
+        case _:
+            bot.send_message(message.from_user.id, "?")
 
 
 bot.polling(none_stop=True, interval=0)
