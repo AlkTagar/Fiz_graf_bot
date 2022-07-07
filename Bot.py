018b0fd8dc62f08a2ad63079b9a0062ea9143571
AlkTagar <r0gozinmax@yandex.ru>
2022-07-07 18:26:13 +0300

    add simple bot

--- Bot.py
+++ Bot.py
@@ -1,1 +1,14 @@
+import telebot
+import config
+
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(content_types=["text"])
+def get_text_message(message):
+    bot.send_message(message.from_user.id, "ЫЫЫЫ")
+
+
+bot.polling(none_stop=True, interval=0)
 
