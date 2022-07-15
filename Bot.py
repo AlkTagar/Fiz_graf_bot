7a348621b18e92a1a417365536db594b5efde20d
AlkTagar <r0gozinmax@yandex.ru>
2022-07-15 14:35:13 +0300

    add input

--- Bot.py
+++ Bot.py
@@ -12,14 +12,37 @@ def start_message(message):
 
 
 @bot.message_handler(commands=["new_graph"])
-def graph(message):
-    bot.send_message(message.from_user.id, "Введите амплитудные значения")
-    return True
+def start_graph(message):
+    bot.send_message(message.from_user.id, "Поочерёдно введите амплитудные значения")
+    bot.send_message(message.from_user.id, "Время в микросекундах t = ")
+    bot.register_next_step_handler(message, get_T)
 
+def get_T(message):
+    global T
+    T = float(message.text)
+    print(f"T= {T}")
+    bot.send_message(message.from_user.id, "Индуктивность катушки в наногенри L = ")
+    bot.register_next_step_handler(message, get_L)
 
-@bot.message_handler(func=graph)
-def input(message):
-    bot.send_message(message.from_user.id, "Введите t")
+def get_L(message):
+    global L
+    L = float(message.text)
+    print(f"L= {L}")
+    bot.send_message(message.from_user.id, "Электроёмкость конденсатора в миллифарадах C = ")
+    bot.register_next_step_handler(message, get_C)
+
+def get_C(message):
+    global C
+    C = float(message.text)
+    print(f"C= {C}")
+    bot.send_message(message.from_user.id, "Сила тока в амперах I = ")
+    bot.register_next_step_handler(message, get_I)
+
+def get_I(message):
+    global I
+    I = float(message.text)
+    print(f"I= {I}")
+    # bot.register_next_step_handler(message, get_L)
 
 
 @bot.message_handler(content_types=["text"])
